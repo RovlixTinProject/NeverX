fb5b4b8e3994dbfec207716f5f4c39087deecb41
RovlixTinProject <maksimverhov1611@gmail.com>
2026-06-20 10:58:20 +0300

    Update subnames.lua

--- subnames.lua
+++ subnames.lua
@@ -3,6 +3,6 @@ satin_ruthless:27.06.2027 17:00
 dyb_sonicalpha2976:27.06.2027 17:00
 nyxarc40:27.06.2027 17:00
 kilerspro1:27.06.2027 17:00
-demonsamadhi:27.06.2027 17:00
+demonsamadhi:01.01.2029 10:00
 chroniko_utsuro141:01.01.2029 10:00
 misato_cyber:01.01.2029 10:00
