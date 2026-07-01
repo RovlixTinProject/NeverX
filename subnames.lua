4e37538b15e98b95141f8dc9ea00e773a8e2f594
RovlixTinProject <maksimverhov1611@gmail.com>
2026-07-01 12:15:01 +0300

    Update subnames.lua

--- subnames.lua
+++ subnames.lua
@@ -1,7 +1,6 @@
 -- ДИСКОРД ЮЗЕРЫ
 
-karl_demonic:01.01.2029 10:00
-satin_ruthless:27.06.2027 17:00
+prajna_yotsuba:27.06.2027 17:00
 dyb_sonicalpha2976:27.06.2027 17:00
 nyxarc40:27.06.2027 17:00
 kilerspro1:27.06.2027 17:00
